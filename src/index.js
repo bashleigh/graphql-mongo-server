7260da47787e4e1cb13e32ab4d54b38f0701e7eb
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2017-12-28 12:33:25 +0000

    Removed root data

--- src/index.js
+++ src/index.js
@@ -2,20 +2,10 @@ import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import schema from './schema';
 
-// const root = {
-//     hello: 'heel',
-//     users: {
-//         id: 1,
-//         firstname: 'test',
-//         lastname: 'tester',
-//     },
-// };
-
 const app = express();
 
 app.use('/', graphqlHTTP({
     schema: schema,
-    //rootValue: root,
     graphiql: true,
 }));
 
